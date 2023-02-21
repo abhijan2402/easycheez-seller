63be4e75438ad7fc85c61f0c360e64e3d29d2f25
abhijan2402 <abhishek.jangid643@gmail.com>
2023-02-21 22:31:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>hi</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
