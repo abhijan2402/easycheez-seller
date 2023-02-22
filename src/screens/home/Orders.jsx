e5f84befddd35ba5a3da8b128c3e2a298361c985
Chirag <takchirag828@gmail.com>
2023-02-22 11:43:15 +0530

    order ui complete

--- src/screens/home/Orders.jsx
+++ src/screens/home/Orders.jsx
@@ -4,7 +4,8 @@ import {
     Text,
     Dimensions,
     StyleSheet,
-    ScrollView
+    ScrollView,
+    Image
 } from 'react-native';
 import OrderButton from '../../components/Home/orderButton';
 import OrderList from '../../components/Home/OrderList';
@@ -13,14 +14,15 @@ const {width,height}=Dimensions.get('window');
 const Orders=()=>{
     
     return (
-        <View style={sytles.container}>
-            <Text style={{color:"black",fontWeight:"bold",fontSize:20,textAlign:"center",padding:10}}>Orders</Text>
+        <ScrollView style={sytles.container}>
+            <Text style={{color:"black",fontWeight:"bold",fontSize:30,textAlign:"center",padding:10}}>Orders</Text>
+            <Image style={{width:width,height:width,resizeMode:"contain"}} source={require("../../assets/imageOrder.png")} />   
             <View style={{flexDirection:"row",width:width,justifyContent:"space-around"}}>
                 <OrderButton title="Active Orders" backgroundcolor={"#F05656"} textColor={"white"} />
                 <OrderButton title="All Orders" backgroundcolor={"#7DFFA2"} textColor={"black"} />
             </View>
             <OrderList />
-        </View>
+        </ScrollView>
     )
 }
 
